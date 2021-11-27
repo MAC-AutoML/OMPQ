28ea6babce00b401d21e4ca2fff29053d9fc3648
BobxmuMa <23020201153788@stu.xmu.edu.cn>
2021-11-27 11:26:46 +0800

    the third commit

--- mixed_bit/run_scripts/quant_mobilenetv2.sh
+++ mixed_bit/run_scripts/quant_mobilenetv2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 python3 -m torch.distributed.launch --nproc_per_node=1 feature_extract.py \
  --model "mobilenetv2" \
- --path "Exp_base/mobilenetv2_base" \
+ --path "/Path/to/Basemodel" \
  --dataset "imagenet" \
- --save_path '/home/data/imagenet' \
+ --save_path '/Path/to/Dataset' \
  --beta 0.000000001 \
  --model_size 0.9 \
  --quant_type "PTQ"
--- mixed_bit/run_scripts/quant_resnet18.sh
+++ mixed_bit/run_scripts/quant_resnet18.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 python3 -m torch.distributed.launch --nproc_per_node=1 feature_extract.py \
  --model "resnet18" \
- --path "Exp_base/resnet18_base" \
+ --path "/Path/to/Basemodel" \
  --dataset "imagenet" \
- --save_path '/home/data/imagenet' \
+ --save_path '/Path/to/Dataset' \
  --beta 100 \
  --model_size 3.0 \
  --quant_type "PTQ"
